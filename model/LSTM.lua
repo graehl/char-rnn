bbae28c452eb56ad0316f643616c70d066c43fbc
graehl <graehl@gmail.com>
2017-06-25 01:05:26 -0700

    retry batch norm

--- model/LSTM.lua
+++ model/LSTM.lua
@@ -1,5 +1,5 @@
 local LSTM = {}
-function LSTM.lstm(input_size, rnn_size, n, dropout)
+function LSTM.lstm(input_size, rnn_size, n, dropout, bn)
     dropout = dropout or 0
 
     -- there will be 2*n+1 inputs
@@ -12,6 +12,14 @@ function LSTM.lstm(input_size, rnn_size, n, dropout)
 
     local x, input_size_L
     local outputs = {}
+    function annotate(x, prefix, bnprefix, L)
+        x = x:annotate{name = (prefix .. L)}
+        if bn then
+            return x:annotate{name = (bnprefix .. L)}
+        else
+            return x
+        end
+    end
     for L = 1,n do
         -- c,h from previos timesteps
         local prev_h = inputs[L*2+1]
@@ -25,9 +33,30 @@ function LSTM.lstm(input_size, rnn_size, n, dropout)
             if dropout > 0 then x = nn.Dropout(dropout)(x) end -- apply dropout, if any
             input_size_L = rnn_size
         end
+
+        -- recurrent batch normalization
+        -- http://arxiv.org/abs/1603.09025
+        local bn_wx, bn_wh, bn_c
+        if bn then
+            bn_wx = nn.BatchNormalization(4 * rnn_size, 1e-5, 0.1, true)
+            bn_wh = nn.BatchNormalization(4 * rnn_size, 1e-5, 0.1, true)
+            bn_c = nn.BatchNormalization(rnn_size, 1e-5, 0.1, true)
+
+            -- initialise beta=0, gamma=0.1
+            bn_wx.weight:fill(0.1)
+            bn_wx.bias:zero()
+            bn_wh.weight:fill(0.1)
+            bn_wh.bias:zero()
+            bn_c.weight:fill(0.1)
+            bn_c.bias:zero()
+        else
+            bn_wx = nn.Identity()
+            bn_wh = nn.Identity()
+            bn_c = nn.Identity()
+        end
         -- evaluate the input sums at once for efficiency
-        local i2h = nn.Linear(input_size_L, 4 * rnn_size)(x):annotate{name='i2h_'..L}
-        local h2h = nn.Linear(rnn_size, 4 * rnn_size)(prev_h):annotate{name='h2h_'..L}
+        local i2h = annotate(nn.Linear(input_size_L, 4 * rnn_size)(x), 'i2h_', 'bn_wx_', L)
+        local h2h = annotate(nn.Linear(rnn_size, 4 * rnn_size)(prev_h), 'h2h_', 'bn_wh_', L)
         local all_input_sums = nn.CAddTable()({i2h, h2h})
 
         local reshaped = nn.Reshape(4, rnn_size)(all_input_sums)
@@ -53,16 +82,18 @@ function LSTM.lstm(input_size, rnn_size, n, dropout)
         end
         local next_c = nn.CAddTable()({
                 nn.CMulTable()({forget_gate, prev_c}),
-                nn.CMulTable()({in_gate,     in_transform})
-                                               })
+                nn.CMulTable()({in_gate, in_transform})
+                                     })
         -- gated cells form the output
         local next_h = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
-
+        if bn then
+            next_h = next_h(bn_c(next_c):annotate {name=('bn_c_' .. L)})
+        end
         table.insert(outputs, next_c)
         table.insert(outputs, next_h)
     end
 
-    -- set up the decoder
+    -- needed for backward / updateGradInput:
     local top_h = outputs[#outputs]
     if dropout > 0 then top_h = nn.Dropout(dropout)(top_h) end
     local proj = nn.Linear(rnn_size, input_size)(top_h):annotate{name='decoder'}
